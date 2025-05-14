9f8536c33303a6c526e5d468dcea82d46ac60999
iiyadh <tabaiiyadh317@gmail.com>
2025-05-14 11:24:25 +0100

    Add Some Unit Tests

---
+++ src/test/java/com/isett/projectexam/controller/BookControllerTestIntegration.java
@@ -0,0 +1,49 @@
+package com.isett.projectexam.controller;
+
+import com.isett.projectexam.model.Book;
+import com.isett.projectexam.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTestIntegration {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setup() {
+        bookRepository.deleteAll();
+        bookRepository.saveAll(Arrays.asList(
+                new Book("111", "Test Book 1", "Test Author 1"),
+                new Book("222", "Test Book 2", "Test Author 2")
+        ));
+    }
+
+    @Test
+    void testGetAllBooksEndpoint() throws Exception {
+        mockMvc.perform(get("/books")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].isbn").value("111"))
+                .andExpect(jsonPath("$[1].isbn").value("222"));
+    }
+
+}
